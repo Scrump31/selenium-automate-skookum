46fdc0d1c1cc226e4998e8dcbce86a908308a0ad
Scottie Crump <sncrump29@gmail.com>
2018-04-15 08:50:02 -0400

    Create openPage API Method * Created method to easily open main pages

--- test/skookumDotCom/page_objects/base_page.js
+++ test/skookumDotCom/page_objects/base_page.js
@@ -1,11 +1,53 @@
 /*eslint no-console: "off"*/
 const fs = require('fs');
+const until = require('selenium-webdriver/lib/until');
 
 const webdriver = require('../../../index');
 class BasePage {
 	constructor() {
 		this.driver = webdriver;
 		this.pageTitle;
+	}
+	async openPage(url) {
+		const base = 'http://www.skookum.com';
+
+		switch (url) {
+		case 'home'.toLowerCase():
+			url = base;
+			break;
+		case 'services'.toLowerCase():
+			url = `${base}/services`;
+			break;
+		case 'work'.toLowerCase():
+			url = `${base}/work`;
+			break;
+		case 'blog'.toLowerCase():
+			url = `${base}/blog`;
+			break;
+		case 'events'.toLowerCase():
+			url = `${base}/events`;
+			break;
+		case 'about'.toLowerCase():
+			url = `${base}/about`;
+			break;
+		case 'contact'.toLowerCase():
+			url = `${base}/contact`;
+			break;
+		default:
+			url = base;
+			break;
+		}
+		this.driver.get(url);
+		this.driver
+			.manage()
+			.window()
+			.maximize();
+		this.pageTitle = await this.getCurrentPageTitle();
+	}
+
+	async verifyPageUpdatedTo(page) {
+		this.driver.wait(until.urlContains(page.toLowerCase()), 3000);
+		this.pageTitle = await this.getCurrentPageTitle();
 	}
 
 	async getCurrentPageTitle() {
