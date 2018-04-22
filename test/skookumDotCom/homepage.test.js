14c317cd2df782d0bf6e9062ea008d7a609f28e8
Scottie Crump <sncrump29@gmail.com>
2018-04-21 22:23:14 -0400

    Refactor Homepage Test File

--- test/skookumDotCom/homepage.test.js
+++ test/skookumDotCom/homepage.test.js
@@ -1,57 +1,57 @@
 const { assert } = require('chai');
 
-const page = require('./page_objects/homepage');
+const homepage = require('./page_objects/homepage');
 
 describe('Skookum.com', function() {
 	it('should open and "Skookum - Strategy, Design, Development" is title of page', async function() {
-		await page.open();
-		
-		assert.strictEqual(page.pageTitle, 'Skookum - Strategy, Design, Development');
+		await homepage.openPage();
+
+		assert.strictEqual(homepage.pageTitle, 'Skookum - Strategy, Design, Development');
 	});
 
 	it('navigates user to Contact Page when "Let\'s Work Together" button clicked ', async function() {
-		await page.open();
-		await page.clickLetsWorkTogetherBtn();
+		await homepage.openPage();
+		await homepage.clickLetsWorkTogetherBtn();
 
-		assert.strictEqual(page.pageTitle, 'Skookum - Contact');
+		assert.strictEqual(homepage.pageTitle, 'Skookum - Contact');
 	});
     
 	it('navigates user to About Page when "Get To Know Us" button clicked ', async function() {
-		await page.open();
-		await page.clickGetToKnowUsBtn();
+		await homepage.openPage();
+		await homepage.clickGetToKnowUsBtn();
 
-		assert.strictEqual(page.pageTitle, 'Skookum - About');
+		assert.strictEqual(homepage.pageTitle, 'Skookum - About');
 	});
     
 	it('navigates user to Services Page when "View Services" button clicked ', async function() {
-		await page.open();
-		await page.clickViewServicesBtn();
+		await homepage.openPage();
+		await homepage.clickViewServicesBtn();
 
-		assert.strictEqual(page.pageTitle, 'Skookum - Services');
+		assert.strictEqual(homepage.pageTitle, 'Skookum - Services');
 	});
     
 	it('navigates user to Premier Case Study when "Explore the case study" button under Premier Inc. clicked ', async function() {
-		await page.open();
-		await page.clickExploreTheCaseStudyPremierBtn();
+		await homepage.openPage();
+		await homepage.clickExploreTheCaseStudyPremierBtn();
 
-		assert.strictEqual(page.pageTitle, 'Case Study - Premier Inc');
+		assert.strictEqual(homepage.pageTitle, 'Case Study - Premier Inc');
 	});
     
 	it('navigates user to Library Case Study when "Explore the case study" button under Charlotte Mecklenburg Library. clicked ', async function() {
-		await page.open();
-		await page.clickExploreTheCaseStudyLibraryBtn();
+		await homepage.openPage();
+		await homepage.clickExploreTheCaseStudyLibraryBtn();
 
-		assert.strictEqual(page.pageTitle, 'Case Study - Charlotte Mecklenburg Library');
+		assert.strictEqual(homepage.pageTitle, 'Case Study - Charlotte Mecklenburg Library');
 	});
     
 	it('navigates user to High Point Case Study when "Explore the case study" button under High Point Market. clicked ', async function() {
-		await page.open();
-		await page.clickExploreTheCaseStudyHighPointBtn();
+		await homepage.openPage();
+		await homepage.clickExploreTheCaseStudyHighPointBtn();
 
-		assert.strictEqual(page.pageTitle, 'Case Study - High Point Market');
+		assert.strictEqual(homepage.pageTitle, 'Case Study - High Point Market');
 	});
 
 	after(async function() {
-		await page.endTest();
+		await homepage.endTest();
 	});
 });
