20afdb6e99cf64d8f9701d616183d3624103b0b9
Scottie Crump <sncrump29@gmail.com>
2019-04-07 21:44:43 -0400

    fix(work): update selectors and assertions

--- skookumDotCom/page_objects/workpage.js
+++ skookumDotCom/page_objects/workpage.js
@@ -1,82 +1,34 @@
 const By = require('selenium-webdriver').By;
 const BasePage = require('./base_page');
+const _ = require('lodash');
 
-// Locators
-const _premierInc_LearnMore_Link =
-	'//h2[text()="Premier Inc: Technology That’s Transforming Healthcare"]';
-const _jri_Shocks_Link = '//h2[text()="JRi Shocks"]';
-const _metal_Marketing_Link = '//h2[text()="Metal Marketing"]';
-const _rj_Reynolds_Tobacco_Link = '//h2[text()="RJ Reynolds Tobacco"]';
-const _cola_Consolidated_Link = '//h2[text()="Coca-Cola Consolidated"]';
-const _electric_Power_Link =
-	'//h2[text()="Electric Power Research Institute (EPRI)"]';
-const _adac_Automotive = '//h2[text()="ADAC Automotive"]';
-
+const _workLinks = [
+	{ h2: 'Service Pros', slug: 'service-pros' },
+	{ h2: 'High Point Market', slug: 'high-point' },
+	{
+		h2: 'Charlotte Mecklenburg Library',
+		slug: 'charlotte-mecklenburg-library',
+	},
+	{
+		h2: 'Premier Inc: Technology That’s Transforming Healthcare',
+		slug: 'premier',
+	},
+	{ h2: 'Coca-Cola Consolidated', slug: 'coca-cola' },
+	{ h2: 'Electric Power Research Institute (EPRI)', slug: 'epri' },
+	{ h2: 'ADAC Automotive', slug: 'adac-automotive' },
+];
 class WorkPage extends BasePage {
 	constructor(driver) {
 		super(driver);
 	}
 
-	// Getter Methods
-	getPremierInc_LearnMore_Link() {
-		return this.driver.findElement(By.xpath(_premierInc_LearnMore_Link));
-	}
-	getJri_Shocks_Link() {
-		return this.driver.findElement(By.xpath(_jri_Shocks_Link));
-	}
-	getMetal_Marketing_Link() {
-		return this.driver.findElement(By.xpath(_metal_Marketing_Link));
-	}
-
-	getRJ_Reynolds_Tobacco_Link() {
-		return this.driver.findElement(By.xpath(_rj_Reynolds_Tobacco_Link));
-	}
-
-	getcola_Consolidated_Link() {
-		return this.driver.findElement(By.xpath(_cola_Consolidated_Link));
-	}
-
-	getElectric_Power_Link() {
-		return this.driver.findElement(By.xpath(_electric_Power_Link));
-	}
-
-	getAdac_Automotive_Link() {
-		return this.driver.findElement(By.xpath(_adac_Automotive));
-	}
-
-	// Action Methods
-	async click_premierInc_LearnMore_Link() {
-		this.getPremierInc_LearnMore_Link().click();
-		await this.verifyPageUpdatedTo('premier');
-	}
-
-	async click_Jri_Shocks_Link() {
-		this.getJri_Shocks_Link().click();
-		await this.verifyPageUpdatedTo('jri-shocks');
-	}
-
-	async click_Metal_Marketing_Link() {
-		this.getMetal_Marketing_Link().click();
-		await this.verifyPageUpdatedTo('metal-marketing');
-	}
-	async click_RJ_Reynolds_Tobacco_Link() {
-		this.getRJ_Reynolds_Tobacco_Link().click();
-		await this.verifyPageUpdatedTo('rj-reynolds');
-	}
-
-	async click_cola_Consolidated_Link() {
-		this.getcola_Consolidated_Link().click();
-		await this.verifyPageUpdatedTo('coca-cola');
-	}
-
-	async click_Electric_Power_Link() {
-		this.getElectric_Power_Link().click();
-		await this.verifyPageUpdatedTo('epri');
-	}
-
-	async click_Adac_Automotive_Link() {
-		this.getAdac_Automotive_Link().click();
-		await this.verifyPageUpdatedTo('adac-automotive');
+	async verify_Work_Links() {
+		const rand = _.random(_workLinks.length - 1);
+		const link = _workLinks[rand];
+		await this.driver
+			.findElement(By.xpath(`//h2[text()="${link.h2}"]`))
+			.click();
+		await this.verifyPageUpdatedTo(link.slug);
 	}
 }
 
--- test/skookumDotCom/workpage.test.js
+++ test/skookumDotCom/workpage.test.js
@@ -9,58 +9,9 @@ describe('Skookum.com/work', function() {
 		assert.strictEqual(workpage.pageTitle, 'Our Work');
 	});
 
-	it('navigates user to Premier Inc Work Page when "Learn More" link clicked ', async function() {
+	it.only('navigates to Work Pages when "Learn More" links clicked ', async function() {
 		await workpage.openPage('work');
-		await workpage.click_premierInc_LearnMore_Link();
-
-		assert.strictEqual(workpage.pageTitle, 'Case Study - Premier Inc');
-	});
-
-	it('navigates user to JRi Shocks Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_Jri_Shocks_Link();
-
-		assert.strictEqual(workpage.pageTitle, 'Case Study - JRi Shocks');
-	});
-
-	it('navigates user to Metal Marketing Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_Metal_Marketing_Link();
-
-		assert.strictEqual(workpage.pageTitle, 'Case Study - Metal Marketing');
-	});
-	it('navigates user to RJ Reynolds Tobacco Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_RJ_Reynolds_Tobacco_Link();
-
-		assert.strictEqual(workpage.pageTitle, 'Case Study - RJ Reynolds Tobacco');
-	});
-
-	it('navigates user to Coca-Cola Consolidated Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_cola_Consolidated_Link();
-
-		assert.strictEqual(
-			workpage.pageTitle,
-			'Case Study - Coca-Cola Consolidated'
-		);
-	});
-
-	it('navigates user to (EPRI) Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_Electric_Power_Link();
-
-		assert.strictEqual(
-			workpage.pageTitle,
-			'Case Study - Electric Power Research Institute (EPRI)'
-		);
-	});
-
-	it('navigates user to ADAC Automotive Work Page when "Learn More" link clicked ', async function() {
-		await workpage.openPage('work');
-		await workpage.click_Adac_Automotive_Link();
-
-		assert.strictEqual(workpage.pageTitle, 'Case Study - ADAC Automotive');
+		await workpage.verify_Work_Links();
 	});
 
 	after(async function() {
